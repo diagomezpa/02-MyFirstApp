fa431b245c2efa421e24b49402d893d7dd6a761e
Diego Alejandro Gomez Parra <diegoalejandro.gomezparra@emeal.nttdata.com>
2024-03-12 14:18:05 -0500

    seccion 4 53 React Native Paper - Instalación

--- App.tsx
+++ App.tsx
@@ -1,13 +1,16 @@
 import {SafeAreaView} from 'react-native';
 import {HelloworldScreen} from './src/presemtation/screens/HelloworldScreen';
 import {CounterScreen} from './src/presemtation/screens/CounterScreen';
+import {PaperProvider} from 'react-native-paper';
 
 export const App = () => {
   return (
-    <SafeAreaView style={{flex: 1}}>
-      {/* <HelloworldScreen name="Diego Gomez" /> */}
-      <HelloworldScreen name="Diego Gomez" />
-      <CounterScreen></CounterScreen>
-    </SafeAreaView>
+    <PaperProvider>
+      <SafeAreaView style={{flex: 1}}>
+        {/* <HelloworldScreen name="Diego Gomez" /> */}
+        <HelloworldScreen name="Diego Gomez" />
+        <CounterScreen></CounterScreen>
+      </SafeAreaView>
+    </PaperProvider>
   );
 };
--- src/presemtation/screens/CounterScreen.tsx
+++ src/presemtation/screens/CounterScreen.tsx
@@ -3,6 +3,7 @@
 import {useState} from 'react';
 import {Platform, Pressable, StyleSheet, Text, View} from 'react-native';
 import {PrimaryButton} from '../components';
+import {Button} from 'react-native-paper';
 
 export const CounterScreen = () => {
   const [count, setCount] = useState<number>(12);
@@ -14,16 +15,23 @@ export const CounterScreen = () => {
     <View style={styles.container}>
       <Text style={styles.title}>{count} </Text>
 
-      <PrimaryButton
+      {/* <PrimaryButton
         label="counter"
         onPress={() => incrementBy(+1)}
-        onLonPress={() => setCount(0)}></PrimaryButton>
+        onLonPress={() => setCount(0)}></PrimaryButton> */}
       {/* <Pressable
         onPress={() => incrementBy(+1)}
         onLongPress={() => setCount(0)}
         style={({pressed}) => [styles.button, pressed && styles.buttonPressed]}>
         <Text style={{color: 'white'}}>+1</Text>
       </Pressable> */}
+
+      <Button
+        mode="contained"
+        onPress={() => incrementBy(+1)}
+        onLongPress={() => setCount(0)}>
+        Incrementar
+      </Button>
     </View>
   );
 };
