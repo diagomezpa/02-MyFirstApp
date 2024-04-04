1a6dab7c8d0df51c7f3a9bbcaea48efc03f7beef
Diego Alejandro Gomez Parra <diegoalejandro.gomezparra@emeal.nttdata.com>
2024-04-04 18:43:51 -0500

    seccion 5 68. Posición absoluta

--- src/presemtation/screens/PositionScreen.tsx
+++ src/presemtation/screens/PositionScreen.tsx
@@ -5,6 +5,7 @@ import {Text} from 'react-native-paper';
 export const PositionScreen = () => {
   return (
     <View style={styles.container}>
+      <View style={styles.greenBox} />
       <View style={styles.purpleBox} />
 
       <View style={styles.orangeBox} />
@@ -16,8 +17,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#28C4D9',
-    justifyContent: 'center',
-    alignItems: 'center',
+    //justifyContent: 'center',
+    //alignItems: 'center',
   },
   purpleBox: {
     width: 100,
@@ -25,8 +26,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#5856D6',
     borderWidth: 10,
     borderColor: 'white',
-    position: 'relative',
-    top: 0,
+    position: 'absolute',
+
+    bottom: 0,
   },
   orangeBox: {
     width: 100,
@@ -34,5 +36,22 @@ const styles = StyleSheet.create({
     backgroundColor: '#F0A23B',
     borderWidth: 10,
     borderColor: 'white',
+    position: 'absolute',
+    top: 0,
+    right: 0,
+  },
+  greenBox: {
+    // flex: 1,
+    // width: 100,
+    // height: 100,
+    backgroundColor: 'green',
+    borderWidth: 10,
+    borderColor: 'white',
+    // position: 'absolute',
+    // top: 0,
+    // left: 0,
+    // bottom: 0,
+    // right: 0,
+    ...StyleSheet.absoluteFillObject,
   },
 });
