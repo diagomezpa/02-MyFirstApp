92cf87add9be619d60ef515a6b19c686b5160e5f
Diego Alejandro Gomez Parra <diegoalejandro.gomezparra@emeal.nttdata.com>
2024-04-04 23:20:29 -0500

    seccion 5 tarea 4

--- src/presemtation/screens/HomeworkScreen.tsx
+++ src/presemtation/screens/HomeworkScreen.tsx
@@ -15,7 +15,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#28425B',
-    justifyContent: 'center',
+    justifyContent: 'space-between',
+    alignItems: 'center',
   },
   box: {
     width: 100,
@@ -34,6 +35,6 @@ const styles = StyleSheet.create({
   },
   blueBox: {
     backgroundColor: '#28C4D9',
-    alignSelf: 'center',
+    alignSelf: 'flex-start',
   },
 });
